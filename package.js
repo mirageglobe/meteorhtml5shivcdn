a644357569ac646f35209c98d23e22cb7ea4ed67
Jimmy MG Lim <mirageglobe@gmail.com>
2014-10-25 14:15:55 +0100

    Fixed client load only for script

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'mirageglobe:html5shivcdn',
   summary: 'CDN base html5shiv for meteorjs',
-  version: '1.0.0',
+  version: '1.0.4',
   git: 'https://github.com/mirageglobe/meteorhtml5shivcdn'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('0.9.4');
-  api.addFiles('mirageglobe:html5shivcdn.js');
+  api.addFiles('mirageglobe:html5shivcdn.js','client');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('mirageglobe:html5shivcdn');
-  api.addFiles('mirageglobe:html5shivcdn-tests.js');
+  api.addFiles('mirageglobe:html5shivcdn-tests.js','client');
 });
