ec4ee5eab2e632f49a92e536a0cef48ecf056b7c
Aaron Gray <aaron@hellometa.com>
2014-11-07 10:55:00 -0700

    Wiat for DOM load and check for > IE 9
    ``#appendData`` gives an error ``undefined is not a function`` and doesn't load the script. 
    
    This new approach creates the IE conditional comment in memory and only builds / embeds the shim if the browser lets us select nodes inside the conditional. 
    
    The ``Meteor.isClient`` scopes this to only run in the browser.
    
    The ``Meteor.startup`` ensures the document is ready before we inject the js.

--- mirageglobe:html5shivcdn.js
+++ mirageglobe:html5shivcdn.js
@@ -1,3 +1,27 @@
-var script = "<!--[if lt IE 9]><script src='//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js'><![endif]-->";
+if (Meteor.isClient) {
+  // Create a new div in memory to test against. Assume
+  // the browser is > IE9 by default.
+  var div = document.createElement("div"),
+      isIeLessThan9 = true;
 
-document.getElementsByTagName('head')[0].appendData(script);
+  // Inject a conditional IE9 comment and see if it
+  // is found.
+  div.innerHTML = "<!--[if lt IE 9]><i></i><![endif]-->";
+
+  // Since the content of the comment is ignored
+  // in other browsers, this will return false in everything
+  // but older IE browsers.
+  isIeLessThan9 = (div.getElementsByTagName("i").length == 1);
+  if (isIeLessThan9) {
+    // Generate a script DOM node and customize attributes.
+    var script  = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src  = '//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js';
+    // Ensure the document is ready with Meteor.startup()
+    Meteor.startup(function() {
+      // Load the script into the document with document.body
+      // because IE doens't make it easy to select the head withoutf jQuery.
+      document.body.appendChild(script);
+    });
+  }
+}
