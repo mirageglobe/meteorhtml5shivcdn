6fc37be6d63eda3d9d263734bcaddbffe516ad78
Jimmy MG Lim <mirageglobe@gmail.com>
2014-11-08 06:48:18 +0000

    updated for jslinting compatibility

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'mirageglobe:html5shivcdn',
   summary: 'CDN base html5shiv for meteorjs',
-  version: '1.0.5',
+  version: '1.0.6',
   git: 'https://github.com/mirageglobe/meteorhtml5shivcdn'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('0.9.4');
-  api.addFiles('mirageglobe:html5shivcdn.js','client');
+  api.addFiles('mirageglobe:html5shivcdn.js', 'client');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('tinytest');
   api.use('mirageglobe:html5shivcdn');
-  api.addFiles('mirageglobe:html5shivcdn-tests.js','client');
+  api.addFiles('mirageglobe:html5shivcdn-tests.js', 'client');
 });
